27e3fdf95495ec356b18fbb35c46a9d0f03a71b3
Ritvik25 <86009242+Ritvik25@users.noreply.github.com>
2022-11-29 19:40:37 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,93 @@
+public class BankAccount {
+	
+	private String name;
+	private String accno;
+	private double balance;
+    private String accno_s,accno_r;
+	BankAccount(String name,String accno,double balance)
+	{
+		this.name = name;
+		this.accno = accno;
+		this.balance = balance;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAccno(){
+		return accno;
+	}
+	
+	public void setAccno(String accno) {
+		this.accno = accno;
+	}
+	
+	public double getBal() {
+		return balance;
+	}
+	
+	public void setBal(double balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(double amount)
+	{
+		System.out.println("Name:"+name);
+		System.out.println("Account number:"+accno);
+		System.out.println("Balance before depositing:"+balance);
+		balance+=amount;
+		System.out.println("Balance after depositing:"+balance);
+		
+	}
+	public void deposit1(double amount)
+	{
+		balance+=amount;
+		System.out.println("Balance after depositing:"+balance);
+		
+	}
+	
+	public void withdraw(double amount)
+	{
+		System.out.println("Name:"+name);
+		System.out.println("Account number:"+accno);
+		if (balance-amount<1000)
+		{
+			System.out.println("Insufficient balance");
+			return;
+		}
+		System.out.println("Balance before withdrawing:"+balance);
+		balance-=amount;
+		System.out.println("Balance after withdrawing:"+balance);
+		
+	}
+	public void withdraw1(double amount)
+	{
+		if (balance-amount<1000)
+		{
+			System.out.println("Insufficient balance");
+			return;
+		}
+		balance-=amount;
+		System.out.println("Balance after withdrawing:"+balance);
+		
+	}
+
+	public void transfer(double amount)
+	{
+		System.out.println("Name:"+name);
+		System.out.println("Account number of sender:"+accno_s);
+        System.out.println("Account number of reciever:"+accno_r);
+        
+		System.out.println("Updated balance after transfering:"+balance);
+		balance-=amount;
+		System.out.println("Updated balance after recieving money:"+balance);
+        return;
+        
+	}
+		
+}
